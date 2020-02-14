3c1d279a23be9d535bb88527977ad3cb4880435c
JosiKie <kiejosi12@gmail.com>
2020-02-14 12:36:32 +0700

    first commit

---
+++ app/src/main/java/com/example/android/intentimplicit/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.intentimplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.buttonGoogle);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://josikie.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
